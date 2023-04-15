9fbff9200325e93684c1d3af86733b04b2073757
Christopher Swinton <chris@theswintons.co.uk>
2023-04-15 21:45:35 +0100

    Re-Created Project from scratch

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Interactive from "./components/interactive";
+
+function App() {
+  return (
+    <main className="Content">
+      <Interactive />
+    </main>
+  );
+}
+
+export default App;
